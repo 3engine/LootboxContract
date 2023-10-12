c225aed00abc0cee482988b395eb154caa67add2
mranoncoder <inbox@abolfazl.de>
2023-10-12 21:56:55 +0200

    📝 added readme and deploy file

--- README.md
+++ README.md
@@ -1,13 +1,55 @@
-# Sample Hardhat Project
+# LootBox and Item NFT Project
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+With this project, you can create and manage LootBoxes and Items, which are NFTs (Non-Fungible Tokens). Additionally, a key mechanism is implemented to facilitate unlocking these LootBoxes.
 
-Try running some of the following tasks:
+This solution is built using [Hardhat](https://hardhat.org/).
 
+## Deployment Instructions
+To deploy the contracts locally, follow these steps:
+
+1. Install the dependencies:
 ```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
+npm install
+```
+
+2. Compile the contracts:
+```shell
+npx hardhat compile
+```
+
+3. Deploy using:
+```shell
 npx hardhat run scripts/deploy.js
 ```
+
+## Deploying to Remote Networks
+If you wish to deploy the contracts to remote networks, such as mainnet or any testnets, you'll need to update your `hardhat.config.js`.
+
+For demonstration, let's consider deploying to the [Polygon Network](https://polygon.technology/):
+
+Add the following network configuration to `hardhat.config.js`:
+```javascript
+module.exports = {
+  networks: {
+    polygon: {
+      url: 'https://polygon-rpc.com/',
+      accounts: [
+        `${PRIVATE_KEY}`,
+      ],
+    }
+  }
+};
+```
+
+Now, deploy to the specified network:
+```shell
+npx hardhat run scripts/deploy.js --network polygon
+```
+
+## Running Tests
+To ensure the robustness of your smart contracts, always test them. Execute tests using:
+
+```shell
+npx hardhat test
+```
+
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
-
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  let LootBox;
+  let ItemContract;
+  let KeyContract;
+  
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  
+  LootBox = await hre.ethers.getContractFactory("LootBoxContract");
+  ItemContract = await hre.ethers.getContractFactory("ItemsContract");
+  KeyContract = await hre.ethers.getContractFactory("KeyContract");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  LootBox = await LootBox.deploy("lootBox","LOOT", deployer.address,"https://test/");
+  await LootBox.waitForDeployment();
+  console.log("LootBox Address: ", LootBox.target);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  ItemContract = await ItemContract.deploy("itemContract", "ITEM","https://test/");
+  await ItemContract.waitForDeployment();
+  console.log("ItemContract Address: ", ItemContract.target);
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  KeyContract = await KeyContract.deploy("KeyContract", "KEY", LootBox.target,"https://test/");
+  await KeyContract.waitForDeployment();
+  console.log("KeyContract Address: ", KeyContract.target);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
